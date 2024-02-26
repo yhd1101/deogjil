47c3d75baf9d0cd9612576d4beac2787207cdac1
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2024-02-26 16:07:15 +0900

    git action 개인 test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -58,8 +58,8 @@ export class AuthController {
 
   @Post('login')
   @ApiOperation({
-    summary: '로그인API',
-    description: '로그인해주는 api입니다 login',
+    summary: '로그인API!!!!!!!!!!!!!!!!!!',
+    description: '로그인해주는 api입니다 login!!!',
   })
   @ApiCreatedResponse({ description: '로그인함', type: User })
   @ApiBody({ type: LoginUserDto })
