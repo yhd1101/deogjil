90538fa0eea0f4619404ef05d021b56ef3c46b24
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2024-03-06 23:42:41 +0900

    도메인설정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -87,12 +87,9 @@ export class AuthService {
       expiresIn: `${this.configService.get('REFRESHTOKEN_EXPIRATION_TIME')}m`,
     });
 
-    // const cookie = `Refresh=${refreshToken}; HttpOnly; path=/; Max-Age=${this.configService.get(
-    //   'REFRESHTOKEN_EXPIRATION_TIME',
-    // )}; SameSite=Lax; Domain=localhost`;
-    const cookie = `Refresh=${refreshToken}; HttpOnly; SameSite=None; path=/; Max-Age=${this.configService.get(
+    const cookie = `Refresh=${refreshToken}; HttpOnly; Secure; path=/; Max-Age=${this.configService.get(
       'REFRESHTOKEN_EXPIRATION_TIME',
-    )}; Domain=localhost`;
+    )}; SameSite=None; Domain=www.dukpool.co.kr`;
 
     return { cookie, refreshToken };
   }
