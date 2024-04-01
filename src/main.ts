3f2910e2d070ba95f7a3bfb05c5fa98bc386620a
양한동 <yhd0121@gs.anyang.ac.kr>
2024-04-01 21:42:31 +0900

    SWAGGER 암호

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { TransformInterceptor } from './common/interceptors/transfrom.intercepto
 import * as path from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import passport from 'passport';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -28,6 +29,16 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalPipes(new ValidationPipe({ transform: true })); //적용을하겠다
+  app.use(
+    //이 부분 추가
+    ['/api-docs'], // docs(swagger end point)에 진입시
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD, // 지정된 ID/비밀번호
+      },
+    }),
+  );
 
   const config = new BaseAPIDocument().initializeOptions();
   const documet = SwaggerModule.createDocument(app, config);
